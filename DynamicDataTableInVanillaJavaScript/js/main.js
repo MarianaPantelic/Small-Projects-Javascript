4460caaa55d48d91801ad001e1035319350cea0c
Mariana Pantelic <mari27line@yahoo.com>
2021-03-04 10:04:26 +0100

    Finished

--- DynamicDataTableInVanillaJavaScript/js/main.js
+++ DynamicDataTableInVanillaJavaScript/js/main.js
@@ -1,18 +1,18 @@
-let data = {
-  headings: [
-    "Name",
-    "Username",
-    "Email",
-    "City",
-    "Phone",
-    "Website",
-    "Company",
-  ],
-  rows: [],
-};
 fetch("https://jsonplaceholder.typicode.com/users")
   .then((response) => response.json())
   .then((results) => {
+    let data = {
+      headings: [
+        "Name",
+        "Username",
+        "Email",
+        "City",
+        "Phone",
+        "Website",
+        "Company",
+      ],
+      data: [],
+    };
     for (let i = 0; i < results.length; i++) {
       let array = [];
       array.push(results[i].name);
@@ -22,28 +22,8 @@ fetch("https://jsonplaceholder.typicode.com/users")
       array.push(results[i].phone);
       array.push(results[i].website);
       array.push(results[i].company.name);
-      data.rows.push(array);
+      data.data.push(array);
     }
+    let myTable = document.getElementById("basic");
+    let dataTable = new DataTable(myTable, { data: data });
   });
-
-console.log(data);
-let myTable = document.getElementById("basic");
-let dataTable = new DataTable(
-  myTable,
-  {
-    perPage: 10,
-    perPageSelect: [5, 10, 15, 20, 25],
-    nextPrev: true,
-    firstLast: false,
-    prevText: "&lsaquo;",
-    nextText: "&rsaquo;",
-    firstText: "&laquo;",
-    lastText: "&raquo;",
-    sortable: true,
-    searchable: true,
-    fixedColumns: true,
-    fixedHeight: false,
-    truncatePager: true,
-  },
-  { data: data }
-);
